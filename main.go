f21c367e67d68fe57b37573959fa367a6f4f150c
Peter Johansson <peter.a.johansson@tele2.com>
2021-05-24 11:06:27 +0200

    new strat

--- main.go
+++ main.go
@@ -1,24 +1,24 @@
 package main
 
 import (
+	"syscall"
 	"time"
 
 	"github.com/getlantern/systray"
-	"github.com/micmonay/keybd_event"
 	"github.com/peterxj1/disable-screenssaver/icon"
-	"github.com/peterxj1/disable-screenssaver/win/mutex"
+)
+
+var (
+	kernel32                    = syscall.NewLazyDLL("kernel32.dll")
+	procSetThreadExecutionState = kernel32.NewProc("SetThreadExecutionState")
 )
 
 const (
-	title = "Disable screensaver"
+	title            = "Disable screensaver"
+	EsSystemRequired = 0x00000001
 )
 
 func main() {
-	id, err := mutex.CreateMutex(title)
-	if err != nil {
-		return
-	}
-	defer mutex.ReleaseMutex(id)
 
 	systray.Run(onReady, nil)
 }
@@ -34,23 +34,13 @@ func onReady() {
 		systray.Quit()
 	}()
 
-	kb, err := keybd_event.NewKeyBonding()
-	if err != nil {
-		panic(err)
+	pulse := time.NewTicker(time.Second * 10)
+
+	for {
+		select {
+		case <-pulse.C:
+			procSetThreadExecutionState.Call(uintptr(EsSystemRequired))
+		}
 	}
 
-	kb.SetKeys(keybd_event.VK_F15)
-
-	ticker := time.NewTicker(59 * time.Second)
-	go func() {
-		for {
-			select {
-			case <-ticker.C:
-				kb.Release()
-			case <-quit.ClickedCh:
-				ticker.Stop()
-				return
-			}
-		}
-	}()
 }
